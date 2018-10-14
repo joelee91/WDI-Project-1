a50e558dc75c0badb36ad1f79a2e56926d65aa5c
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-14 16:30:31 -0400

    created snake body

--- snake.js
+++ snake.js
@@ -3,23 +3,53 @@ var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-setInterval(game, 100);
+// setInterval(game, 1000);
 
-
+var snakeHead;
 var snake = [];
 var speed = [
 	{x:speedX, y:speedY}
 ]
 var speedX;
 var speedY;
-var positionX;
-var positionY;
-var snakeTail = 4;
+var positionX = 10;
+var positionY = 100;
+var width = 9;
+var height = 9;
+var snakeTail = 9;
 
 class DrawSnake  {
 	constructor(x, y, width, height){
-		this.x = positionX;
-		this.y = positionY
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	context.beginPath();
+	context.fillRect(this.x, this.y, width, height);
+	context.stroke();
 	}
 }
 
+function snakeBody () {
+	for (i = 0; i < snakeTail; i++) {
+		snake.push ({x:positionX, y:positionY})
+	}
+}
+
+function addBody () {
+	for (i = 0; i < snake.length; i++) {
+		snakeHead = new DrawSnake(positionX * i, positionY, width, height);
+	}
+}
+
+// function moveRight() {
+// 	snake.push({x:positionX, y:positionY});
+// }
+
+
+function game() {
+// moveRight();
+snakeBody();
+addBody();
+}
+game();
