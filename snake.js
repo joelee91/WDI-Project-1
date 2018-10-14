edb54cf109e8c94a07072ffa64d4c51348dee2c6
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-14 12:04:33 -0400

    tested function in snake.js

--- snake.js
+++ snake.js
@@ -3,3 +3,13 @@ var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+var snake = [];
+
+function snakeBody() {
+	for (i = 0; i < 3; i++) {
+		context.fillRect(i*100, i*100, 100, 100)
+		context.stroke();
+	}
+}
+
+snakeBody();
