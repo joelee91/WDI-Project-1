ad80fcd0561c12c9adc1168525e2a866edaaf25e
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-14 15:54:59 -0400

    restarted code

--- snake.js
+++ snake.js
@@ -5,3 +5,21 @@ canvas.height = window.innerHeight;
 
 setInterval(game, 100);
 
+
+var snake = [];
+var speed = [
+	{x:speedX, y:speedY}
+]
+var speedX;
+var speedY;
+var positionX;
+var positionY;
+var snakeTail = 4;
+
+class DrawSnake  {
+	constructor(x, y, width, height){
+		this.x = positionX;
+		this.y = positionY
+	}
+}
+
