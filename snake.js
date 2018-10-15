f05e065dcb3b64c9a3861a55776cad97e8ec0e6b
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-14 20:57:46 -0400

    created snake body

--- snake.js
+++ snake.js
@@ -7,11 +7,25 @@ canvas.height = window.innerHeight;
 var snake = [];
 var xVelocity = 0;
 var yVelocity = 0;
-var width = 9;
-var height = 9;
+var width = 20;
+var height = 20;
 var tail = 4
+
 function snakeBody() {
-	for (var i = tail; i > 0; i++){
-		snake.push(x:i +)
+	for (var i = tail; i > 0; i--){
+		snake.push({x:i, y:200})
 	}
 }
+
+function snakePaint() {
+	for (var j = 0; j < snake.length; j++){
+		context.fillRect(snake[j].x * 22, snake[j].y, width, height)
+		
+	}
+}
+
+function game() {
+
+}
+snakeBody();
+snakePaint();
