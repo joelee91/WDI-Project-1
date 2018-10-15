8676fdc7d1feb5ec9bd62ef90f277cce4997ed6f
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-14 20:34:48 -0400

    Deleted current code and starting new canvas

--- snake.js
+++ snake.js
@@ -3,53 +3,15 @@ var context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// setInterval(game, 1000);
 
-var snakeHead;
 var snake = [];
-var speed = [
-	{x:speedX, y:speedY}
-]
-var speedX;
-var speedY;
-var positionX = 10;
-var positionY = 100;
+var xVelocity = 0;
+var yVelocity = 0;
 var width = 9;
 var height = 9;
-var snakeTail = 9;
-
-class DrawSnake  {
-	constructor(x, y, width, height){
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-	context.beginPath();
-	context.fillRect(this.x, this.y, width, height);
-	context.stroke();
+var tail = 4
+function snakeBody() {
+	for (var i = tail; i > 0; i++){
+		snake.push(x:i +)
 	}
 }
-
-function snakeBody () {
-	for (i = 0; i < snakeTail; i++) {
-		snake.push ({x:positionX, y:positionY})
-	}
-}
-
-function addBody () {
-	for (i = 0; i < snake.length; i++) {
-		snakeHead = new DrawSnake(positionX * i, positionY, width, height);
-	}
-}
-
-// function moveRight() {
-// 	snake.push({x:positionX, y:positionY});
-// }
-
-
-function game() {
-// moveRight();
-snakeBody();
-addBody();
-}
-game();
