f0ae32e3bf73c1e2f93f5e3c8594ce666d351da9
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-10-15 05:44:25 -0400

    created movement keys

--- snake.js
+++ snake.js
@@ -10,6 +10,9 @@ var yVelocity = 0;
 var width = 20;
 var height = 20;
 var tail = 4
+var appleX;
+var appleY;
+setInterval(snakePaint, 1000);
 
 function snakeBody() {
 	for (var i = tail; i > 0; i--){
@@ -17,14 +20,34 @@ function snakeBody() {
 	}
 }
 
+
 function snakePaint() {
 	for (var j = 0; j < snake.length; j++){
 		context.fillRect(snake[j].x * 22, snake[j].y, width, height)
-		
 	}
-}
-
-function game() {
+	// context.fillStyle = ('white');
+	// context.fillRect(0, 0, innerWidth, innerHeight);
+	var headX = snake[0].x;
+	var headY = snake[0].y;
+	headX += xVelocity;
+	headY += yVelocity;
+	document.onkeydown = function (event) {
+		switch (event.keyCode) {
+			case 37:
+				xVelocity--;
+				break;
+			case 38:
+				yVelocity--;
+				break;
+			case 39:
+				xVelocity++;
+				break;
+			case 40:
+				yVelocity++;
+				break;
+			
+		}
+	}
 
 }
 snakeBody();
